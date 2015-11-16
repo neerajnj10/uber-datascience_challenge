1d07dc0221b2501adde18f74a6ca1e1ecef487b9
Neeraj Kumar <nekumar@mix.wvu.edu>
2015-11-16 03:36:00 -0500

    Update uber-data_science.py

--- uber-data_science.py
+++ uber-data_science.py
@@ -1,1 +1,10 @@
+##python code for uber-data-science_challenge
 
+import pandas as pd
+import json
+path = (r"C:\Users\Nj_neeraj\Documents\Data_Science\uber_data_challenge.json")
+text = open(path,'r')
+x = text.read()
+y =json.loads(x)
+data = pd.DataFrame(y)
+#print(data.head)
