1c644e18e1929f4f0c9851fc2d615f04f234088a
Neeraj Kumar <nekumar@mix.wvu.edu>
2015-11-25 08:48:24 -0500

    Update uber-data_science.py

--- uber-data_science.py
+++ uber-data_science.py
@@ -2,9 +2,20 @@
 
 import pandas as pd
 import json
+import matplotlib.pyplot as plt
+
 path = (r"C:\Users\Nj_neeraj\Documents\Data_Science\uber_data_challenge.json")
 text = open(path,'r')
 x = text.read()
 y =json.loads(x)
 data = pd.DataFrame(y)
 #print(data.head)
+uber= data.dropna() #remove missing values
+print(len(uber)) #check the length now
+print(uber.dtypes) #datatypes
+headers = uber.dtypes.index #column names
+print(headers)
+coruber = pd.DataFrame.corr(uber)
+print(coruber)
+plt.matshow(coruber) #correlation plot
+print(len(headers))
